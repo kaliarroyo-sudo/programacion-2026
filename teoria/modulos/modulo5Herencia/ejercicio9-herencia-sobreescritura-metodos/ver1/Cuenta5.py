f093ae519e3d9e49a097df739fa807e699db9fff
kaliarroyo-sudo <kaliarroyo@ciencias.unam.mx>
2026-06-04 20:40:37 -0600

    Add files via upload

---
+++ teoria/modulos/modulo5Herencia/ejercicio9-herencia-sobreescritura-metodos/ver1/Cuenta5.py
@@ -0,0 +1,30 @@
+"""
+Created on 2026
+@author: kaliarroyo-sudo
+
+Clase Cuenta:
+Ejemplo de atributo privado vs público.
+"""
+
+class Cuenta:
+    def __init__(self, valor):
+        # atributo privado: no accesible directamente desde fuera
+        self.__cantidad = valor
+
+    def depositar(self, valor):
+        if valor > 0:
+            self.__cantidad += valor
+        else:
+            print("El valor para depositar es erróneo.")
+
+    def retirar(self, valor):
+        if valor > 0 and valor <= self.__cantidad:
+            self.__cantidad -= valor
+        else:
+            print("Fondos insuficientes o monto inválido.")
+
+    def mostrarDetalles(self):
+        print("Saldo actual:", self.__cantidad)
+
+    def __str__(self):
+        return f"Cuenta | Saldo: {self.__cantidad}"
